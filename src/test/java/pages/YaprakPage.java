e566a3cd9c51d0526a45d474b70484a8578df79d
yaprak <ersanyaprakk@gmail.com>
2026-03-04 14:00:50 +0100

    yaprak/04.03.2026/14:00

--- src/test/java/pages/YaprakPage.java
+++ src/test/java/pages/YaprakPage.java
@@ -14,7 +14,7 @@ public class YaprakPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    // us15
+    // US15
     @FindBy(xpath = "//a[@href='/organizations']")
     public WebElement allOrganizationsBtn;
     @FindBy(xpath = "(//span[contains(@class,'swiper-pagination-bullet')])[last()]")
@@ -23,5 +23,69 @@ public class YaprakPage {
     public WebElement coursesTab;
     @FindBy(xpath = "//*[contains(text(),\"Instructor doesn't have any course!\")]")
     public List<WebElement> noCourseMessageList;
+
+
+    //US43
+    @FindBy(xpath = "(//a[@href='/login'])[1]")
+    public WebElement ilkLoginButonu;
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement emailKutusu;
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement passwordKutusu;
+    @FindBy(xpath = "//button[text()='Login']")
+    public WebElement submitLoginButonu;
+    @FindBy(xpath = "//span[normalize-space()='Dashboard']")
+    public WebElement dashboardAltMenu;
+    // Filtre tarih alanları (From / To)
+    @FindBy(name = "from")
+    public WebElement fromDateInput;
+    @FindBy(name = "to")
+    public WebElement toDateInput;
+    @FindBy(xpath = "//button[@type='submit' and contains(.,'Show Results')]")
+    public WebElement showResultsButton;
+    @FindBy(xpath = "//h2[contains(normalize-space(),'No result')]")
+    public WebElement noResult;
+    @FindBy(xpath = "//div[contains(@class,'table-responsive')]//tr")
+    public List<WebElement> alinmayanSinavlar;
+    @FindBy(xpath = "//span[text()=\"Quizzes\"]")
+    public WebElement QuizzesMenu;
+    @FindBy(xpath = "//*[text()=\"My results\"]")
+    public WebElement myResultsMenu;
+    @FindBy(xpath = "//*[text()=\"Not Participated\"]")
+    public  WebElement notParticipatedMenu;
+    @FindBy(xpath = "//h2[@class='section-title' and normalize-space()='Results statistics']")
+    public WebElement resultsStatisticsTitle;
+
+    //US45
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
 
